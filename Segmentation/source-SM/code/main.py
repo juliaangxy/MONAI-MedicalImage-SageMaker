f3839a1291bd42f98e55d97eb617f61678a085c6
juliaangxy <juliaangxy@gmail.com>
2022-04-15 15:16:54 +0800

    Create main.py

---
+++ Segmentation/source-SM/code/main.py
@@ -0,0 +1,309 @@
+from __future__ import absolute_import
+
+import sys
+import os
+import shutil
+import tempfile
+import time
+import matplotlib.pyplot as plt
+import numpy as np
+from monai.apps import DecathlonDataset, download_and_extract
+from monai.config import print_config, DtypeLike, PathLike
+from monai.data import CacheDataset, DataLoader, Dataset, decollate_batch
+from monai.data.image_reader import ImageReader, ITKReader, NibabelReader, NumpyReader, PILReader
+from monai.data.nifti_saver import NiftiSaver
+from monai.handlers.utils import from_engine
+from monai.losses import DiceLoss
+from monai.inferers import sliding_window_inference
+from monai.metrics import DiceMetric
+from monai.networks.nets import SegResNet, UNet
+from monai.transforms import (
+    Activations,
+    Activationsd,
+    AsDiscrete,
+    AsDiscreted,
+    EnsureChannelFirstd,
+    Compose,
+    CropForegroundd,
+    Invertd,
+    LoadImaged,
+    MapTransform,
+    NormalizeIntensityd,
+    Orientationd,
+    RandCropByPosNegLabeld,
+    RandFlipd,
+    RandScaleIntensityd,
+    RandShiftIntensityd,
+    RandSpatialCropd,
+    SaveImaged,
+    ScaleIntensityRanged,
+    Spacingd,
+    EnsureChannelFirstd,
+    EnsureTyped,
+    EnsureType,
+    Invertd,
+)
+from monai.utils import first, set_determinism, optional_import
+# from monai.utils import InterpolateMode, OptionalImportError, ensure_tuple, look_up_option, optional_import
+import torch
+import tempfile
+import shutil
+import glob
+import argparse
+
+## parse arguments 
+def arg_parse():
+#     """Acquire hyperparameters and directory locations passed by SageMaker"""
+    parser = argparse.ArgumentParser()
+
+    # Hyperparameters sent by the client are passed as command-line arguments to the script.
+    parser.add_argument("--epochs", type=int, default=100)
+    parser.add_argument("--train_batch_size", type=int, default=2)
+    parser.add_argument("--val_batch_size", type=int, default=1)
+    parser.add_argument("--val_interval", type=int, default=2)
+    parser.add_argument("--num_workers", type=int, default=2)
+
+    # Data, model, and output directories
+    parser.add_argument("--output-data-dir", type=str, default=os.environ.get("SM_OUTPUT_DATA_DIR"))
+    parser.add_argument("--model-dir", type=str, default=os.environ.get("SM_MODEL_DIR"))
+    
+    # sagemaker-containers passes hyperparameters as arguments
+#     parser.add_argument("--epoch", type=int, default=500)
+#     parser.add_argument("--batch", type=int, default=1)
+
+    # This is a way to pass additional arguments when running as a script
+    # and use sagemaker-containers defaults to set their values when not specified.
+    parser.add_argument("--train", type=str, default=os.environ.get("SM_CHANNEL_TRAIN"))
+    parser.add_argument("--validation", type=str, default=os.environ.get("SM_CHANNEL_VALIDATION"))    
+
+#     parser.add_argument("--train", type=str, default=os.environ.get("SM_CHANNEL_TRAIN"))
+#     parser.add_argument("--test", type=str, default=os.environ.get("SM_CHANNEL_TEST"))
+    return parser.parse_args()
+#     return parser.parse_known_args()
+
+
+hyperparameters_file_path = "/opt/ml/input/config/hyperparameters.json"
+inputdataconfig_file_path = "/opt/ml/input/config/inputdataconfig.json"
+resource_file_path = "/opt/ml/input/config/resourceconfig.json"
+data_files_path = "/opt/ml/input/data/"
+failure_file_path = "/opt/ml/output/failure"
+model_artifacts_path = "/opt/ml/model/"
+
+training_job_name_env = "TRAINING_JOB_NAME"
+training_job_arn_env = "TRAINING_JOB_ARN"
+
+# load the dataset. firstly, we need to define transformer for both training dataset and validation dataset
+
+def train_transforms(data):
+    
+    Compose(
+        [
+            LoadImaged(keys=["image", "label"]),
+            EnsureChannelFirstd(keys=["image", "label"]),
+            Orientationd(keys=["image", "label"], axcodes="RAS"),
+            Spacingd(keys=["image", "label"], pixdim=(
+                1.5, 1.5, 2.0), mode=("bilinear", "nearest")),
+            ScaleIntensityRanged(
+                keys=["image"], a_min=-57, a_max=164,
+                b_min=0.0, b_max=1.0, clip=True,
+            ),
+            CropForegroundd(keys=["image", "label"], source_key="image"),
+            RandCropByPosNegLabeld(
+                keys=["image", "label"],
+                label_key="label",
+                spatial_size=(96, 96, 96),
+                pos=1,
+                neg=1,
+                num_samples=4,
+                image_key="image",
+                image_threshold=0,
+            ),
+            # user can also add other random transforms
+            # RandAffined(
+            #     keys=['image', 'label'],
+            #     mode=('bilinear', 'nearest'),
+            #     prob=1.0, spatial_size=(96, 96, 96),
+            #     rotate_range=(0, 0, np.pi/15),
+            #     scale_range=(0.1, 0.1, 0.1)),
+            EnsureTyped(keys=["image", "label"]),
+        ]
+    )
+def val_transforms(data):
+    Compose(
+        [
+            LoadImaged(keys=["image", "label"]),
+            EnsureChannelFirstd(keys=["image", "label"]),
+            Orientationd(keys=["image", "label"], axcodes="RAS"),
+            Spacingd(keys=["image", "label"], pixdim=(
+                1.5, 1.5, 2.0), mode=("bilinear", "nearest")),
+            ScaleIntensityRanged(
+                keys=["image"], a_min=-57, a_max=164,
+                b_min=0.0, b_max=1.0, clip=True,
+            ),
+            CropForegroundd(keys=["image", "label"], source_key="image"),
+            EnsureTyped(keys=["image", "label"]),
+        ]
+    )
+
+    
+# def data_dicts(zip_files_folder):
+
+#     return dicts
+
+def load_data(data_dir, inputdata, batch_size, num_workers, source):
+    
+    images = sorted(glob.glob(os.path.join(data_dir, source, "imagesTr", "*.nii.gz")))
+    labels = sorted(glob.glob(os.path.join(data_dir, source, "labelsTr", "*.nii.gz")))
+    
+    data_files = [
+    {"image": image_name, "label": label_name}
+    for image_name, label_name in zip(images, labels)
+    ]
+    
+    ##training dataset
+    if(source=='train'):
+        transforms = train_transforms(inputdata)
+#         train_batch_size = int(os.environ['SM_HP_TRAIN-BATCH-SIZE'])
+#         train_batch_size = args.train_batch_size
+        train_batch_size = batch_size
+        train_ds = CacheDataset(data=data_files, transform=transforms, cache_rate=0.8,num_workers=num_workers)
+#         train_loader = DataLoader(train_ds, batch_size=2, shuffle=True, num_workers=4)
+        loader = DataLoader(train_ds, batch_size=batch_size, shuffle=True, num_workers=num_workers)
+        ##training dataset
+
+    if(source=='val'):
+        val_transforms = val_transforms(inputdata)
+#         val_batch_size = int(os.environ['SM_HP_VAL-BATCH-SIZE'])
+#         val_batch_size = args.val_batch_size
+        val_batch_size = batch_size
+        val_ds = CacheDataset(data=data_files, transform=transforms, cache_rate=0.8, num_workers=num_workers)
+        # val_ds = Dataset(data=val_files, transform=val_transforms)
+#         val_loader = DataLoader(val_ds, batch_size=1, num_workers=4)
+        loader = DataLoader(val_ds, batch_size=batch_size, num_workers=num_workers)
+
+    return loader
+#     return x_train, y_train, x_test, y_test, input_shape, n_labels
+
+
+
+def training(epochs, val_interval, train_loader, validation_loader):
+    
+    # standard PyTorch program style: create UNet, DiceLoss and Adam optimizer
+    device = torch.device("cuda:0")
+    ## definition of model
+    model = UNet(
+        spatial_dims=3,
+        in_channels=1,
+        out_channels=2,
+        channels=(16, 32, 64, 128, 256),
+        strides=(2, 2, 2, 2),
+        num_res_units=2,
+        norm=Norm.BATCH,
+    ).to(device)
+    loss_function = DiceLoss(to_onehot_y=True, softmax=True)
+    optimizer = torch.optim.Adam(model.parameters(), 1e-4)
+    dice_metric = DiceMetric(include_background=False, reduction="mean")
+    
+
+    ## model training
+#     max_epochs = 50 #600
+#     max_epochs = int(os.environ['SM_HP_EPOCHS'])
+#     max_epochs = args.epoch
+    max_epochs = epochs
+#     val_interval = 2
+#     val_interval = int(os.environ['SM_HP_VAL-INTERVAL'])
+#     val_interval = args.val_interval
+    val_interval = val_interval
+    best_metric = -1
+    best_metric_epoch = -1
+    epoch_loss_values = []
+    metric_values = []
+    post_pred = Compose([EnsureType(), AsDiscrete(argmax=True, to_onehot=2)])
+    post_label = Compose([EnsureType(), AsDiscrete(to_onehot=2)])
+
+    for epoch in range(max_epochs):
+        print("-" * 10)
+        print(f"epoch {epoch + 1}/{max_epochs}")
+        model.train()
+        epoch_loss = 0
+        step = 0
+        for batch_data in train_loader:
+            step += 1
+            inputs, labels = (
+                batch_data["image"].to(device),
+                batch_data["label"].to(device),
+            )
+            optimizer.zero_grad()
+            outputs = model(inputs)
+            loss = loss_function(outputs, labels)
+            loss.backward()
+            optimizer.step()
+            epoch_loss += loss.item()
+            print(
+                f"{step}/{len(train_ds) // train_loader.batch_size}, "
+                f"train_loss: {loss.item():.4f}")
+        epoch_loss /= step
+        epoch_loss_values.append(epoch_loss)
+        print(f"epoch {epoch + 1} average loss: {epoch_loss:.4f}")
+
+        if (epoch + 1) % val_interval == 0:
+            model.eval()
+            with torch.no_grad():
+                for val_data in val_loader:
+                    val_inputs, val_labels = (
+                        val_data["image"].to(device),
+                        val_data["label"].to(device),
+                    )
+                    roi_size = (160, 160, 160)
+                    sw_batch_size = 4
+                    val_outputs = sliding_window_inference(
+                        val_inputs, roi_size, sw_batch_size, model)
+                    val_outputs = [post_pred(i) for i in decollate_batch(val_outputs)]
+                    val_labels = [post_label(i) for i in decollate_batch(val_labels)]
+                    # compute metric for current iteration
+                    dice_metric(y_pred=val_outputs, y=val_labels)
+
+                # aggregate the final mean dice result
+                metric = dice_metric.aggregate().item()
+                # reset the status for next validation round
+                dice_metric.reset()
+
+                metric_values.append(metric)
+                if metric > best_metric:
+                    best_metric = metric
+                    best_metric_epoch = epoch + 1
+                    torch.save(model.state_dict(), os.path.join(
+                        root_dir, "best_metric_model.pth"))
+                    print("saved new best metric model")
+                print(
+                    f"current epoch: {epoch + 1} current mean dice: {metric:.4f}"
+                    f"\nbest mean dice: {best_metric:.4f} "
+                    f"at epoch: {best_metric_epoch}"
+                )
+
+    ## save the model 
+#     model_dir = os.environ["SM_MODEL_DIR"]
+    save_model_artifacts(model_dir + "/", net)
+
+
+if __name__ == "__main__":
+    
+    parser = argparse.ArgumentParser()
+    # Hyperparameters sent by the client are passed as command-line arguments to the script.
+#     parser.add_argument("--epochs", type=int, default=100)
+#     parser.add_argument("--train-batch-size", type=int, default=2)
+#     parser.add_argument("--val-batch-size", type=int, default=1)
+#     parser.add_argument("--val-interval", type=int, default=2)
+#     # This is a way to pass additional arguments when running as a script
+#     # and use sagemaker-containers defaults to set their values when not specified.
+#     parser.add_argument("--train", type=str, default=os.environ["SM_CHANNEL_TRAIN"])
+#     parser.add_argument("--validation", type=str, default=os.environ["SM_CHANNEL_VALIDATION"])  
+    args = arg_parse()
+#     args = parser.parse_args()
+#     train_batch = int(os.environ['SM_HP_TRAIN-BATCH-SIZE'])
+#     train_data_files = data_dicts("train")
+#     val_data_files = data_dicts("val")
+    
+    train_loader = load_data(data_files_path, args.train, args.train_batch_size, args.num_workers, "train")
+    val_loader = load_data(data_files_path, args.val, args.val_batch_size, args.num_workers, "val")
+    training(args.epochs, args.val_interval, train_loader, val_loader)
